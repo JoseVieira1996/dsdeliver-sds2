b6c4fe0aad144552c9d49c97af1c61c47bb5c7fa
JoseVieira1996 <jose.adm@outlook.com>
2021-01-09 02:05:48 -0300

    Create front-web-initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Hello from './Hello';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
